1ab01c313c290e503dbc4ea6379f5092d8bbf4e6
Girum-Tamirat <girumtamirat10@gmail.com>
2024-09-12 19:36:28 +0300

    Foundation first-commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
